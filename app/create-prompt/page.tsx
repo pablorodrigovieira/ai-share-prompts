fa23bb054689b555ac89af49dffadbef71f7633a
Pablo Vieira <pablovieira.con@gmail.com>
2023-09-09 16:56:10 +1000

    feat: add prompt component and POST route

---
+++ app/create-prompt/page.tsx
@@ -0,0 +1,53 @@
+'use client'
+import React, { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import Form from '@components/Form';
+
+const CreatePrompt = () => {
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt:'',
+    tag: ''
+  })
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    try {
+      const res = await fetch('/api/prompt/new', {
+        method: 'POST',
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+          userId: session?.user.id
+        })
+      })
+      console.log('res', res)
+      if(res && res.ok){
+        router.push('/')
+      }
+    } catch (e) {
+      console.log('e', e);
+    } finally {
+      setSubmitting(false);
+    }
+  }
+
+  return (
+    <div>
+      <Form
+        type="Create"
+        post={post}
+        submitting={submitting}
+        setPost={setPost}
+        handleSubmit={createPrompt}
+      />
+    </div>
+  );
+};
+
+export default CreatePrompt;
--- components/Form.tsx
+++ components/Form.tsx
@@ -1,10 +1,77 @@
 import React from 'react';
+import Link from 'next/link';
 
-const Form = () => {
+interface IPost {
+  prompt: string;
+  tag: string;
+}
+
+interface IForm {
+  type: string;
+  post: IPost;
+  submitting: boolean;
+  setPost: (post: IPost) => void;
+  handleSubmit: (e) => void
+}
+
+const Form = (props: IForm) => {
+  const { type, post, submitting, setPost, handleSubmit } = props;
   return (
-    <div>
-      Form
-    </div>
+    <section className="w-full max-w-full flex-start flex-col">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{type} Prompt</span>
+      </h1>
+      <p className="desc text-left max-w-md">
+        {type} and share your AI prompts with the World.
+      </p>
+      <form
+        onSubmit={handleSubmit}
+        className="mt-10 w-full max-w-2x1 flex flex-col gap-7 glassmorphism"
+      >
+        <label>
+          <span className="font-satoshi font-semibold text-base text-gray-700">
+            Your AI Prompt
+          </span>
+          <textarea
+            value={post.prompt}
+            onChange={(e) => setPost({
+              ...post, prompt: e.target.value
+            })}
+            required
+            placeholder="Write your prompt here..."
+            className="form_textarea"
+          />
+        </label>
+        <label>
+          <span className="font-satoshi font-semibold text-base text-gray-700">
+            Tag {` `}
+            <span className="font-normal">
+              (#product, #web, #idea)
+            </span>
+          </span>
+          <input
+            value={post.tag}
+            onChange={(e) => setPost({
+              ...post, tag: e.target.value
+            })}
+            placeholder="#tag"
+            className="form_input"
+          />
+        </label>
+
+        <div className="flex-end mx-3 mb-5 gap-4">
+          <Link href="/" className="text-gray-500-text-sm">Cancel</Link>
+          <button
+            type="submit"
+            disabled={submitting}
+            className="px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white"
+          >
+            {submitting ? `${type}...` : type}
+          </button>
+        </div>
+
+      </form>
+    </section>
   );
 };
 
